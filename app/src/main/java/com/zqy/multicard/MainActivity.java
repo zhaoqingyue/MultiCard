6d6e414dad64502c022bd3ec5facae8b0a1871d4
zhaoqy <zhaoqy@careland.com.cn>
2018-05-04 09:30:10 +0800

    update

--- app/src/main/java/com/zqy/multicard/MainActivity.java
+++ app/src/main/java/com/zqy/multicard/MainActivity.java
@@ -3,22 +3,25 @@ package com.zqy.multicard;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 
     @OnClick({R.id.normal})
-    @Override
     public void onClick(View v){
         switch (v.getId()) {
             case R.id.normal: {
+                Toast.makeText(this, "点击效果", Toast.LENGTH_SHORT).show();
                 break;
             }
         }
